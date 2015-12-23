1d59d388df0fe48951c0bac101ca7da2a6240179
bengisua <bengisu.arisoy@bilgiedu.net>
2015-12-23 22:51:21 +0200

    documentation updated

---
+++ README.md
@@ -0,0 +1,1 @@
+This software advise people to select a lecture from their likess
--- lecturerecommendation/lecturerecommendation/admin.py
+++ lecturerecommendation/lecturerecommendation/admin.py
@@ -1,4 +1,5 @@
 from django.contrib import admin
-from .models import TrainData
+from .models import TrainData,Lectures
 
 admin.site.register(TrainData)
+admin.site.register(Lectures)
