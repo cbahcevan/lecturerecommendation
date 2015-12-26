10cf9c5a2f3468708ef3c371c8a337904c3bed82
cbahcevan <anil.bahcevan@bilgiedu.net>
2015-12-27 01:57:46 +0200

    model hatalar duzeltme

--- lecturerecommendation/lecturerecommendation/admin.py
+++ lecturerecommendation/lecturerecommendation/admin.py
@@ -2,9 +2,9 @@ from django.contrib import admin
 from .models import TrainData,Lectures
 
 admin.site.register(TrainData)
-<<<<<<< HEAD
+
 
 admin.site.register(Lectures)
-=======
-admin.site.register(Lectures)
->>>>>>> origin/master
+
+
+
--- lecturerecommendation/lecturerecommendation/views.py
+++ lecturerecommendation/lecturerecommendation/views.py
@@ -8,18 +8,18 @@ import recommend
 This function translates form input into data which we can evaluate
 and predict using sklearn
 """
-<<<<<<< HEAD
+
 def getFromLectures(lecid):
     L = Lectures.objects.get(lecid=lecid)
     return L.lecexp
-=======
+
 
 def loadLectureDetails(req):
 	print req[0]
 	expla = Lectures.objects.get(lecid=str(req[0]))
 	result = expla.lecture_explanation
 	return result
->>>>>>> origin/master
+
 
 def organizeFormData(req):
 	organized = map(lambda x:req[x],req)
@@ -39,17 +39,16 @@ def main(request):
     	userData = organizeFormData(request.POST)
         d = preparedata.fitDataToPandas(TrainData.objects.all())
         result= recommend.evaluate(d[0],d[1],userData)
-<<<<<<< HEAD
+
         explanation = getFromLectures(str(result[0]))
         print str(result[0])
         
 
         return render(request, 'main.html', {'rec':explanation})      
-=======
+
 
 
 
         exp =  loadLectureDetails(result)
         return render(request, 'main.html', {'rec':str(exp)})
->>>>>>> origin/master
     return render(request, 'main.html', {'form':form})
