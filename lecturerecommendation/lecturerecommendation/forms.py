61ee88af6cd9a5b34213422a82ed94634dafdfdb
canyergoc <can.yergoc@bilgiedu.net>
2015-12-27 12:34:03 +0200

    questions updated

--- lecturerecommendation/lecturerecommendation/forms.py
+++ lecturerecommendation/lecturerecommendation/forms.py
@@ -9,9 +9,9 @@ class DForms(forms.Form):
   love = (
     (1, 'Yes'),
     (0, 'No'),)
-  math = forms.ChoiceField(choices = ((1,"Yes"),(0,"No")), label="do you love math", initial='', widget=forms.Select(), required=True)
+  math = forms.ChoiceField(choices = ((1,"Yes"),(0,"No")), label="do you love solving problems", initial='', widget=forms.Select(), required=True)
   read = forms.ChoiceField(choices = ((1,"Yes"),(0,"No")), label="do you love reading", initial='', widget=forms.Select(), required=True)
   lit = forms.ChoiceField(choices = ((1,"Yes"),(0,"No")), label="do you love literature", initial='', widget=forms.Select(), required=True)
-  high = forms.ChoiceField(choices = ((1,"Yes"),(0,"No")), label="before grades high", initial='', widget=forms.Select(), required=True)
+  high = forms.ChoiceField(choices = ((1,"Yes"),(0,"No")), label="do you still love your high school lectures ?", initial='', widget=forms.Select(), required=True)
   music = forms.ChoiceField(choices = ((1,"Yes"),(0,"No")), label="do you love music", initial='', widget=forms.Select(), required=True)
-  politics = forms.ChoiceField(choices = ((1,"Yes"),(0,"No")), label="do you love politics", initial='', widget=forms.Select(), required=True)
+  politics = forms.ChoiceField(choices = ((1,"Yes"),(0,"No")), label="do you interested in politics", initial='', widget=forms.Select(), required=True)
